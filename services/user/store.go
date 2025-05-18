29e777a8c21831095b174ae84e2eb54060167bcf
d.fathi <d.fathi@kian.digital>
2025-05-18 16:10:08 +0330

    setup GetUserByEmail method

--- services/user/store.go
+++ services/user/store.go
@@ -26,6 +26,29 @@ func (s *Store) CreateUser(user types.User) error {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.DB.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+	u := new(types.User)
+
+	if rows.Next() {
+		err := rows.Scan(
+			&u.ID,
+			&u.FirstName,
+			&u.LastName,
+			&u.Email,
+			&u.Password,
+			&u.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found.")
+	}
+	return u, nil
 
 }
 func (s *Store) GetUserByID(id int) (*types.User, error) {}
